35624c7631723259afe1eec93929529550940175
tushar <iktushar2020@gmail.com>
2025-05-26 23:06:12 +0600

    onAuthStateChanged on authprovider

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../../Firebase/Firebase.init";
 
 const AuthProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -12,17 +17,29 @@ const AuthProvider = ({ children }) => {
       setLoading(false)
     );
   };
-  
-  const signInUser = (email,password) => {
+
+  const signInUser = (email, password) => {
     setLoading(true);
-    return signInWithEmailAndPassword(auth,email,password)
-  }
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const authInfo = {
     createUser,
     signInUser,
+    user,
     loading,
   };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      console.log("user in the auth state change", currentUser);
+    });
+    return () => {
+      unSubscribe();
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
