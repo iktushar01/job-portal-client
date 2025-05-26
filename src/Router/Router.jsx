32b080f7ec3c833977e1a30ec17482ba5f54ad6c
tushar <iktushar2020@gmail.com>
2025-05-26 12:32:24 +0600

    setup repo and add navbar footer router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout/>,
+    children: [
+        {
+            index: true,
+            element: <Home/>
+        }
+    ]
+  },
+]);
+
+export default router;
