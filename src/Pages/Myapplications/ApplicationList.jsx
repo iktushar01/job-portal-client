b5640c9f4cd2a369e84370d3fc9e0e2f4cc2b1ea
tushar <iktushar2020@gmail.com>
2025-05-28 22:58:46 +0600

    ApplicationState

---
+++ src/Pages/Myapplications/ApplicationList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ApplicationList = () => {
+    return (
+        <div>
+            hi
+        </div>
+    );
+};
+
+export default ApplicationList;
--- src/Pages/Myapplications/Myapplications.jsx
+++ src/Pages/Myapplications/Myapplications.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ApplicationState from './applicationState';
+import ApplicationList from './ApplicationList';
 
 const Myapplications = () => {
     return (
         <div>
             <ApplicationState></ApplicationState>
+            <ApplicationList></ApplicationList>
         </div>
     );
 };
